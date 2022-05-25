7996837fa8befbb449c31d8118043653b3a82051
owentr1369 <tamvaa1306@gmail.com>
2022-05-25 23:23:59 +0700

    setup

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    Hello
+  </div>
+</template>
+
+<script>
+// Tat ca du lieu truyen qua html o dang du lieu tho
+// computed thuc hien nhu mot function nhung chi duoc coi la 1 t
+export default {
+  name: "App",
+  data() {
+
+  },
+  methods: {},
+  computed: {
+  
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.vuong {
+  width: 100px;
+  height: 100px;
+  background-color: gray;
+  display: inline-block;
+  margin: 10px;
+}
+</style>
